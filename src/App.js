964fc290a5b79dae9a03b59772a3c0a84a9994bb
devopsasp <85219013+devopsasp@users.noreply.github.com>
2022-04-29 18:00:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import OrderDetails from './CoffeeComp/orderdetails';
+function App() {
+  return (
+    <div className="App">
+      <OrderDetails />
+       </div>
+  );
+}
+
+export default App;
---
+++ src/CoffeeComp/orderdetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import AcceptOrder from "./acceptorder"
+import './coffee.css'
+export default class OrderDetails  extends React.Component{
+
+    render()
+    {
+        return (<div></div>)
+    }
+
+    }
